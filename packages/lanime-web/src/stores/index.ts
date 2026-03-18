a696e772652f7c99155ae1c1dd3795495f789634
Cho MnSeong <chemins60217@gmail.com>
2026-03-18 09:14:05 +0900

    Feature: Redux 루트 스토어에 사용자 프로필 리듀서 통합

--- packages/lanime-web/src/stores/index.ts
+++ packages/lanime-web/src/stores/index.ts
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { episodeModalReducer } from "./episodeModal";
+import { userProfileReducer } from "./auth";
 
 export const store = configureStore({
   reducer: {
     episodeModal: episodeModalReducer,
+    userProfile: userProfileReducer
   },
 });
+
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
