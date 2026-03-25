6566149d565da5020d25523db9a04b2509e141eb
Cho MnSeong <chemins60217@gmail.com>
2026-03-25 09:56:28 +0900

    Refactor: 쿼리 클라이언트 초기화 로직 분리

--- packages/lanime-web/server/entry-client.tsx
+++ packages/lanime-web/server/entry-client.tsx
@@ -4,11 +4,10 @@ import { Provider } from 'react-redux'
 import { store } from '../src/stores'
 import { hydrateRoot, createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '../src/libs/apis/queryClient'
 
 const container = document.getElementById('root') as HTMLElement
-
-const queryClient = new QueryClient()
 
 const app = (
     <StrictMode>
